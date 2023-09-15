463fa0c668c06c0d69266caf36e5497504c0a573
Nacho Albiol <nacho23@pluralcamp.com>
2023-09-15 12:01:23 +0200

    Update Routing (Home + Profile)

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import ProfileInfo from './components/ProfileInfo';
-import ButtonRow from './components/ButtonRow';
-import AboutMe from './components/AboutMe';
-import Hobbies from './components/Hobbies';
-import CardContainer from './components/CardContainer';
-import Footer from './components/Footer';
+import ProfileInfo from '../components/ProfileInfo';
+import ButtonRow from '../components/ButtonRow';
+import AboutMe from '../components/AboutMe';
+import Hobbies from '../components/Hobbies';
+import CardContainer from '../components/CardContainer';
 
-import './css/Style.css';
-import './css/Profile.css';
+import '../components/css/Profile.css';
 
 function ProfilePage() {
   return (
     <div>
-      <Navbar />
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-md-6">
@@ -27,7 +23,6 @@ function ProfilePage() {
           </div>
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
