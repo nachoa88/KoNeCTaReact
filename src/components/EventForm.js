721fc7abf5a4cd88df25c0f7298f6861496048d5
Nacho Albiol <nacho23@pluralcamp.com>
2023-10-06 12:42:35 +0200

    Update EventForm

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -12,8 +12,11 @@ export default function CreateEventPage() {
         eventType: "",
         description: "",
         startingDate: "",
+        finishingDate: "",
         startingTime: "",
+        finishingTime: "",
         location: "",
+        img: null,
     });
 
     /* Función para manejar el cambio en el imput y asignar el valor dependiendo del tipo de data (eventType, location, etc).
@@ -29,6 +32,15 @@ export default function CreateEventPage() {
             [name]: value,
         });
     }
+
+    //  ESTO ES LO QUE HABIA HECHO HASTA EL MOMENTO DE LA IMG
+    const handleImageInputChange = (event) => {
+        const file = event.target.files[0];
+        setData({
+            ...data,
+            img: file, // Update the image property with the selected image file
+        });
+    };
 
     // Se llama a la función de dataService para agregar el event.
     const addEvent = () => {
@@ -47,36 +59,45 @@ export default function CreateEventPage() {
                         <div className="form-group">
                             <fieldset>
                                 <label htmlFor="eventName" className="formlabel">Event name</label>
-                                <input type="text" id="eventName" name="eventName" placeholder="Nombre del evento" size="30"
+                                <input type="text" id="eventName" name="eventName" placeholder="Write the name of the Event"
                                     required className="form-control" value={data.eventName} onChange={handleInputChange} />
-                                <label htmlFor="eventType" className="formlabel">Event type</label>
-                                <select id="eventType" name="eventType" required className="form-control" value={data.eventType} onChange={handleInputChange}>
-                                    <option value="Arte Urbano">Arte Urbano</option>
-                                    <option value="Cultural">Cultural</option>
-                                    <option value="Art">Art</option>
-                                    <option value="Danza">Danza</option>
-                                    <option value="Rock">Rock</option>
-                                    <option value="Comedia">Comedia</option>
-                                </select>
-                                <img src="img/UploadImage.jpg" alt="..." id="upload" />
-                                <label htmlFor="description" className="formlabel">Description</label>
-                                <input type="textarea" id="description" name="description" size="30" placeholder="Describe brevemente el evento"
-                                    required className="form-control" value={data.description} onChange={handleInputChange} />
+                                <label htmlFor="startingDate" className="formlabel">Starting Date</label>
+                                <input type="date" id="startingDate" name="startingDate"
+                                    required className="form-control" value={data.startingDate} onChange={handleInputChange} />
+                                <label htmlFor="finishingDate" className="formlabel">Finishing Date</label>
+                                <input type="date" id="finishingDate" name="finishingDate"
+                                    required className="form-control" value={data.finishingDate} onChange={handleInputChange} />
+                                <label htmlFor="startingTime" className="formlabel">Starting Hour</label>
+                                <input type="time" id="startingTime" name="startingTime"
+                                    required className="form-control" value={data.startingTime} onChange={handleInputChange} />
+                                <label htmlFor="finishingTime" className="formlabel">Finishing Hour</label>
+                                <input type="time" id="finishingTime" name="finishingTime"
+                                    required className="form-control" value={data.finishingTime} onChange={handleInputChange} />
                             </fieldset>
                         </div>
                         <div className="form-group">
                             <fieldset>
-                                <label htmlFor="startingDate" className="formlabel">Starting Date</label>
-                                <input type="date" id="startingDate" name="startingDate" required className="form-control" value={data.startingDate} onChange={handleInputChange} />
-                                <label htmlFor="finishingDate" className="formlabel">Finishing Date</label>
-                                <input type="date" id="finishingDate" name="finishingDate" required className="form-control" value={data.finishingDate} onChange={handleInputChange} />
-                                <label htmlFor="startingTime" className="formlabel">Starting Hour</label>
-                                <input type="time" id="startingTime" name="startingTime" required className="form-control" value={data.startingTime} onChange={handleInputChange} />
-                                <label htmlFor="finishingTime" className="formlabel">Finishing Hour</label>
-                                <input type="time" id="finishingTime" name="finishingTime" required className="form-control" value={data.finishingTime} onChange={handleInputChange} />
+                                <label htmlFor="eventType" className="formlabel">Event type</label>
+                                <select id="eventType" name="eventType"
+                                    required className="form-control" value={data.eventType} onChange={handleInputChange}>
+                                    <option value="Art">Art</option>
+                                    <option value="Comedy">Comedy</option>
+                                    <option value="Cultural">Cultural</option>
+                                    <option value="Dance">Dance</option>
+                                    <option value="Pop">Pop</option>
+                                    <option value="Rock">Rock</option>
+                                    <option value="Theater">Theater</option>
+                                    <option value="Urban Art">Urban Art</option>
+                                </select>
+                                <label htmlFor="img" className="formlabel">Upload Image</label>
+                                <input type="file" id="uploadImg" name="uploadImg" accept="image/*" className="form-control" onChange={handleImageInputChange} />
+                                <label htmlFor="description" className="formlabel" >Description</label>
+                                <input type="textarea" id="description" name="description" placeholder="Breif description of the event"
+                                    required className="form-control" value={data.description} onChange={handleInputChange} />
                                 <label htmlFor="location" className="formlabel">Location</label>
-                                <input type="text" id="location" name="location" size="30" placeholder="Lugar del evento" required className="form-control" value={data.location} onChange={handleInputChange} />
-                                <img src="img/map.jpg" alt="..." id="minimap" />
+                                <input type="text" id="location" name="location" placeholder="Write the location of the event"
+                                    required className="form-control" value={data.location} onChange={handleInputChange} />
+                                <img src="img/map.jpg" alt="Location in the map" id="minimap" />
                                 <div className="form-signin-footer">
                                     <NavLink to="/" className="btn btn-lg btn-primary btn-block" id="save" type="submit" onClick={addEvent}>Save</NavLink>
                                 </div>
