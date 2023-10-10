602c7a226bf9ab11d5e1b2688c6d1c15249a36e8
Nacho Albiol <nacho23@pluralcamp.com>
2023-10-10 22:52:14 +0200

    Responsive Update

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,24 +1,22 @@
 export default function FAQ() {
     return (
-        <div className="container faq">
-            <div className="row">
-                <div>
-                    <h1>FAQ - preguntas frecuentes</h1>
-                    <p className="question"><strong>1. ¿Qué es esta aplicación de eventos culturales y para quién está destinada?</strong></p>
-                    <p className="answer">Esta aplicación es una plataforma diseñada específicamente para jóvenes que buscan eventos culturales gratuitos y alternativos en su área. Está dirigida a adolescentes que desean explorar la escena cultural "underground" y descubrir eventos que se adapten a sus intereses.</p>
-                    <p className="question"><strong>2. ¿Cuál es la intención detrás de esta iniciativa?</strong></p>
-                    <p className="answer">La iniciativa nace de la pregunta: ¿cómo podemos acercar a los adolescentes a la cultura de una manera práctica y sencilla? Nuestra intención es proporcionar una solución a este desafío al ofrecer una plataforma que facilita la búsqueda y el acceso a eventos culturales relevantes para los jóvenes</p>
-                    <p className="question"><strong>3. ¿Cómo se utiliza esta aplicación en general?</strong></p>
-                    <p className="answer">La aplicación te permite buscar eventos culturales gratuitos en tu área. Puedes explorar eventos en forma de lista o en un mapa interactivo. Además, puedes personalizar tus búsquedas según tus intereses al completar tu perfil de usuario con tus preferencias culturales.</p>
-                    <p className="question"><strong>4. ¿La aplicación es gratuita?</strong></p>
-                    <p className="answer">Sí, la aplicación es completamente gratuita para todos los usuarios. No hay costos asociados con el uso de la plataforma ni con la creación de un perfil.</p>
-                    <p className="question"><strong>5. ¿Qué responsabilidad asume este proyecto?</strong></p>
-                    <p className="answer">Este proyecto tiene como único propósito facilitar el acceso a la información sobre eventos culturales gratuitos. No asume responsabilidad por los eventos en sí ni por las interacciones entre usuarios. Cada evento es responsabilidad de su organizador, y los usuarios deben ejercer su propio juicio al asistir a ellos.</p>
-                    <p className="question"><strong>6. ¿Quiénes están detrás de este proyecto?</strong></p>
-                    <p className="answer">Este proyecto es desarrollado por un grupo de estudiantes de desarrollo de aplicaciones con tecnología web. Nuestro objetivo es crear una herramienta útil y amigable para la comunidad de jóvenes que desean explorar la cultura local de una manera divertida y segura.</p>
-                    <p className="question"><strong>7. ¿Qué podemos esperar en el futuro de esta aplicación?</strong></p>
-                    <p className="answer">Estamos trabajando constantemente para mejorar la experiencia de nuestros usuarios. Pronto añadiremos nuevas funciones, como la posibilidad de indicar que vas a asistir a eventos, la interacción entre usuarios, un mapa completamente funcional y un sistema de recompensas (tokens) que desbloqueará características especiales en la plataforma para recompensar la participación activa en la comunidad. ¡Estamos emocionados por lo que viene!</p>
-                </div>
+        <div className="container" id="faq">
+            <div>
+                <h1>FAQ - preguntas frecuentes</h1>
+                <p className="question"><strong>1. ¿Qué es esta aplicación de eventos culturales y para quién está destinada?</strong></p>
+                <p className="answer">Esta aplicación es una plataforma diseñada específicamente para jóvenes que buscan eventos culturales gratuitos y alternativos en su área. Está dirigida a adolescentes que desean explorar la escena cultural "underground" y descubrir eventos que se adapten a sus intereses.</p>
+                <p className="question"><strong>2. ¿Cuál es la intención detrás de esta iniciativa?</strong></p>
+                <p className="answer">La iniciativa nace de la pregunta: ¿cómo podemos acercar a los adolescentes a la cultura de una manera práctica y sencilla? Nuestra intención es proporcionar una solución a este desafío al ofrecer una plataforma que facilita la búsqueda y el acceso a eventos culturales relevantes para los jóvenes</p>
+                <p className="question"><strong>3. ¿Cómo se utiliza esta aplicación en general?</strong></p>
+                <p className="answer">La aplicación te permite buscar eventos culturales gratuitos en tu área. Puedes explorar eventos en forma de lista o en un mapa interactivo. Además, puedes personalizar tus búsquedas según tus intereses al completar tu perfil de usuario con tus preferencias culturales.</p>
+                <p className="question"><strong>4. ¿La aplicación es gratuita?</strong></p>
+                <p className="answer">Sí, la aplicación es completamente gratuita para todos los usuarios. No hay costos asociados con el uso de la plataforma ni con la creación de un perfil.</p>
+                <p className="question"><strong>5. ¿Qué responsabilidad asume este proyecto?</strong></p>
+                <p className="answer">Este proyecto tiene como único propósito facilitar el acceso a la información sobre eventos culturales gratuitos. No asume responsabilidad por los eventos en sí ni por las interacciones entre usuarios. Cada evento es responsabilidad de su organizador, y los usuarios deben ejercer su propio juicio al asistir a ellos.</p>
+                <p className="question"><strong>6. ¿Quiénes están detrás de este proyecto?</strong></p>
+                <p className="answer">Este proyecto es desarrollado por un grupo de estudiantes de desarrollo de aplicaciones con tecnología web. Nuestro objetivo es crear una herramienta útil y amigable para la comunidad de jóvenes que desean explorar la cultura local de una manera divertida y segura.</p>
+                <p className="question"><strong>7. ¿Qué podemos esperar en el futuro de esta aplicación?</strong></p>
+                <p className="answer">Estamos trabajando constantemente para mejorar la experiencia de nuestros usuarios. Pronto añadiremos nuevas funciones, como la posibilidad de indicar que vas a asistir a eventos, la interacción entre usuarios, un mapa completamente funcional y un sistema de recompensas (tokens) que desbloqueará características especiales en la plataforma para recompensar la participación activa en la comunidad. ¡Estamos emocionados por lo que viene!</p>
             </div>
         </div>
     )
